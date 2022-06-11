488882ce6f92334a7698bc72b2d6e4d274bec0df
Marilena Sales <marilena.sales@gmail.com>
2022-06-11 20:49:27 -0300

    funcoes

---
+++ Funcoes/switch.js
@@ -0,0 +1,14 @@
+function getAnimal(id){
+  switch(id){
+    case 1:
+      return "cão";
+      case 2:
+      return "gato";
+      case 1:
+      return "pássaro";
+      default:
+        return "peixe";
+  }
+}
+
+getAnimal(1);
